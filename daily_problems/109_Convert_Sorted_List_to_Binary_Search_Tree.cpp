95b3258f49d32ff9f01a1d0df5a774aeaffe6496
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2023-03-11 22:23:54 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/109_Convert_Sorted_List_to_Binary_Search_Tree.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    TreeNode* chk(int i, int j, vector<int>& v){
+
+        if(i>j){
+            return NULL;
+        }
+        int mid=i+(j-i)/2;
+        TreeNode* root=new TreeNode(v[mid]);
+        root->left=chk(i, mid-1, v);
+        root->right=chk(mid+1, j, v);
+        return root;
+    }
+    TreeNode* sortedListToBST(ListNode* head) {
+
+        vector<int>v;
+        while(head){
+            v.push_back(head->val);
+            head=head->next;
+        }
+        int i=0, j=v.size();
+        return chk(i, j-1, v);
+    }
+};
