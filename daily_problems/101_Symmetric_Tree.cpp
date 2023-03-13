ae45c14d9ce80a7e61212482797ee72a4e5525ea
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2023-03-13 23:12:29 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/101_Symmetric_Tree.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool helper(TreeNode* left,TreeNode* right){
+
+        if(!left and !right)
+            return true;
+        if((left != NULL and right == NULL) || (left == NULL and right != NULL) || (left->val!=right->val))
+            return false;
+        if(left->val==right->val)
+            return helper(left->right,right->left) && helper(left->left,right->right);
+        return true;
+    }
+    
+    bool isSymmetric(TreeNode* root) {
+
+        if(!root)
+            return true;
+        return helper(root->left,root->right);
+    }
+};
