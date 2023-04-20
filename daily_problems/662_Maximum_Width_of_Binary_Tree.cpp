2d64b43563905698a7e236181b726587fb178434
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2023-04-20 23:28:34 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/662_Maximum_Width_of_Binary_Tree.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+
+        if(root==NULL)
+            return 0;
+        long long ans=0;
+        queue<pair<TreeNode*, int>>q;
+        q.push({root, 0});
+        while(!q.empty()){
+            int l=q.size();
+            long long la=q.back().second, f=q.front().second;
+            for(int i=0; i<l; i++){
+                TreeNode* temp;
+                temp=q.front().first;
+                long long ind=q.front().second;
+                q.pop();   
+                if(temp->left!=NULL){q.push({temp->left, ind*2+1});}
+                if(temp->right!=NULL){q.push({temp->right, ind*2+2});}
+            }
+            ans=max(ans, la-f+1);
+        }
+        return ans;
+    }
+};
