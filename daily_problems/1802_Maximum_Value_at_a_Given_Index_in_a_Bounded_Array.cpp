ec5f94e71c5e5f811412e6e8cbdb7aa63dc09212
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2023-06-10 23:08:54 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/1802_Maximum_Value_at_a_Given_Index_in_a_Bounded_Array.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    typedef long long ll;
+    
+    ll getSumElements(ll count, ll val) {
+        
+        return val*count - (count*(count+1))/2;
+        
+    }
+    
+    int maxValue(int n, int index, int maxSum) {
+        
+        ll left  = 1;
+        ll right = maxSum;
+        
+        ll mid_val;
+        int result = 0;
+        
+        
+        while(left <= right) {
+            
+            mid_val = left + (right - left)/2;
+            
+            ll left_count = min((ll)index, mid_val-1);
+            
+            ll left_sum  = getSumElements(left_count, mid_val);
+            
+            left_sum += max((ll)0, index - mid_val+1);
+            
+            
+            ll right_count = min((ll)n-index-1, mid_val-1);
+            
+            ll right_sum  = getSumElements(right_count, mid_val);
+            
+            right_sum += max((ll)0, n-index-1 - mid_val+1);
+            
+            
+            
+            if(left_sum + right_sum + mid_val <= maxSum) {
+                result = max((ll)result, mid_val);
+                
+                left = mid_val+1;
+            } else {
+                right = mid_val-1;
+            }
+            
+        }
+        
+        
+        return result;
+        
+    }
+};
