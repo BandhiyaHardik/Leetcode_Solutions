65599971b188c8f562974d8ae67a3f106ca00a2e
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2023-06-17 22:50:41 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/1187_Make_Array_Strictly_Increasing.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    map<pair<int, int>, int> mp;
+    
+    int solve(int idx, vector<int>& arr1, vector<int>& arr2, int prev) {
+        if(idx == arr1.size())
+            return 0;
+        
+        int result1 = 1e9+1;
+        
+        if(mp.find({idx, prev}) != mp.end())
+            return mp[{idx, prev}];
+        
+        if(arr1[idx] > prev) {
+            result1 = solve(idx+1, arr1, arr2, arr1[idx]);
+        }
+        
+        int result2 = 1e9+1;
+        auto it = upper_bound(begin(arr2), end(arr2), prev);
+        
+        if(it != end(arr2)) {
+            int i = it - begin(arr2);
+            result2 = 1 + solve(idx+1, arr1, arr2, arr2[i]);
+        }
+        
+        return mp[{idx, prev}] = min(result1, result2);
+        
+        
+    }
+    
+    int makeArrayIncreasing(vector<int>& arr1, vector<int>& arr2) {
+        sort(begin(arr2), end(arr2));
+        mp.clear();
+        
+        int result = solve(0, arr1, arr2, INT_MIN);
+        
+        if(result == 1e9+1)
+            return -1;
+        
+        return result;
+    }
+};
