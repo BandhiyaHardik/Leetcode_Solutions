f6d7b1e80df9ce76988e0f84676b833d4a9df929
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2023-06-26 22:52:51 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/2462_Total_Cost_to_Hire_K_Workers.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    long long totalCost(vector<int>& costs, int k, int candidates) {
+        int n = costs.size();
+        
+        priority_queue<int,vector<int>,greater<int>> pq1,pq2;
+        
+        long long ans = 0;
+        
+        int hired = 0;
+        int i = 0;
+        int j = n-1;
+        
+        while(hired < k){
+            
+            while(pq1.size() < candidates && i<=j) 
+                pq1.push(costs[i++]);
+            while(pq2.size()<candidates && j>=i) 
+                pq2.push(costs[j--]);
+            
+            
+            int a = pq1.size() > 0 ? pq1.top() : INT_MAX;
+            int b = pq2.size() > 0 ? pq2.top() : INT_MAX;
+            
+            if(a <= b){
+                ans += a;
+                pq1.pop();
+            } else {
+                ans += b;
+                pq2.pop();
+            }
+            
+            hired++;
+        }
+        
+        return ans;
+    }
+};
