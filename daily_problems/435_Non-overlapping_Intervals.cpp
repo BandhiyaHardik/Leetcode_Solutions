572c3e4b168fc33671d97c36a3aba15635575482
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2023-07-19 23:31:51 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/435_Non-overlapping_Intervals.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+
+        sort(intervals.begin(), intervals.end());
+        vector<int> prev=intervals[0];
+        int num=-1;
+        for(vector<int>& A: intervals){
+            if (A[0]<prev[1] && A[1]<=prev[1]){
+                num++;
+                prev=A;
+            }
+            else if(A[0]<prev[1] && A[1]>prev[1])
+                num++;
+            else 
+                prev=A;
+        }
+        return num;      
+    }
+};
