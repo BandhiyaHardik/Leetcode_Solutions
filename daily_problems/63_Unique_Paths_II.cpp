0ad93161f733756aac6bcf68010d764ca772dc7e
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2023-08-12 22:44:38 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/63_Unique_Paths_II.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+
+        int n = obstacleGrid[0].size();
+
+        vector<vector<int>>dp(m, vector<int>(n));
+
+        dp[0][0] = 1;
+
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) {
+            return 0;
+        }
+
+        for (int i = 0; i < m; ++i) {
+
+            for (int j = 0; j < n; ++j) {
+                
+                if (obstacleGrid[i][j] == 1) {
+                    continue;
+                }
+                else {
+                    if (i > 0) {
+                        dp[i][j] +=dp[i-1][j];
+                    }
+                    if (j > 0) {
+                        dp[i][j] +=dp[i][j-1];
+                    }
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
