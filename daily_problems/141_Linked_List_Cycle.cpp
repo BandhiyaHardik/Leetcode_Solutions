1818b84c5375e4217fd7ff59b17781779af3bd02
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2023-09-04 22:19:28 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/141_Linked_List_Cycle.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        if (!head || !head->next) {
+            return false;
+        }
+
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while (slow != fast) {
+            if (!fast || !fast->next) {
+                return false;
+            }
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return true;
+    }
+};
