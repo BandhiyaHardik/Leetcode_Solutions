209b1691f58a072a06ba2ac146a71864f27df4bc
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2023-10-30 00:52:44 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/458_Poor_Pigs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int T = minutesToTest/minutesToDie + 1;
+
+        int pigs = 0;
+
+        while(pow(T, pigs) < buckets) {
+            pigs++;
+        }
+
+        return pigs;
+       
+    }
+};
