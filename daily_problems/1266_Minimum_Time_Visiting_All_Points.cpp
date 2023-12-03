1b11d068ac426589f27a04ddbe9113410ef6df39
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2023-12-03 23:21:11 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/1266_Minimum_Time_Visiting_All_Points.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        
+        int steps = 0;
+        
+        for(int i = 0; i<n-1; i++) { 
+            int x1 = points[i][0];
+            int y1 = points[i][1];
+            
+            int x2 = points[i+1][0];
+            int y2 = points[i+1][1]; 
+            
+            int dx = abs(x2-x1);
+            int dy = abs(y2-y1);
+            
+            
+            int diagonal = min(dx, dy);
+            int remain   = abs(dx-dy);
+            
+            
+            steps += diagonal + remain;
+            
+        }
+        
+        return steps;
+        
+    }
+};
