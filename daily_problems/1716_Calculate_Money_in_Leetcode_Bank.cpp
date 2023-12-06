8f803326a1d58c1d9804459dcd9365ebf2a31a90
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2023-12-06 23:52:26 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/1716_Calculate_Money_in_Leetcode_Bank.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int totalMoney(int n) {
+        int result = 0;
+        
+        int monday_money = 1;
+        
+        while(n > 0) {
+            
+            int money = monday_money;
+            for(int day = 1; day <= min(n, 7); day++) {
+                result += money++;
+            }
+            
+            
+            n -= 7;
+            monday_money += 1;
+        }
+        
+        return result;
+        
+    }
+};
