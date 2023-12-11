8bbda0fedac9f5ee2a84e025a57fca6a64ebb1be
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2023-12-11 23:17:33 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/1287_Element_Appearing_More_Than_25%_In_Sorted_Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findSpecialInteger(vector<int>& arr) {
+        int n = arr.size();
+        int freq = n/4;
+        
+        for(int i = 0; i<n-freq; i++) {
+          
+            if(arr[i] == arr[i+freq]) {
+                return arr[i];
+            }   
+          
+        }
+        return -1;
+    }
+};
