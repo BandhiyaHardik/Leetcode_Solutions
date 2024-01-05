d9e01ebe6192cb1f796a6add69b869c9cd1eb95f
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-01-05 23:25:35 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/300_Longest_Increasing_Subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> t(n, 1);
+        int maxL = 1;
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<i; j++) {
+                if(nums[j] < nums[i]) {
+                    t[i] = max(t[i], t[j]+1);
+                    maxL = max(maxL, t[i]);
+                }
+            }
+        }
+        
+        return maxL;
+    }
+};
