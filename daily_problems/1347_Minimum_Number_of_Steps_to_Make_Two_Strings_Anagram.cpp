2b0840c33084a95198be3d8f6bf7441b2645069e
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-01-13 22:06:44 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/1347_Minimum_Number_of_Steps_to_Make_Two_Strings_Anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minSteps(string s, string t) {
+        int n = s.length();
+        
+        int mp[26] = {0};
+        
+        for(int i = 0; i<n; i++) {
+            mp[s[i] - 'a']++;
+            mp[t[i] - 'a']--;
+        }
+
+        int result = 0;
+
+        for(int i = 0; i < 26; i++) {
+            result += max(0, mp[i]);
+        } 
+        
+        return result;
+    }
+};
