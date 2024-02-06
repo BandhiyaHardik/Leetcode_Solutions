4f974627a9e8cd99fab910efe8e8c03bc4e3355d
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-02-06 23:13:33 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/387_First_Unique_Character_in_a_String.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int arr[26] = {0};
+        
+        for(char &ch : s) {
+            arr[ch-'a']++;
+        }
+        
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(arr[s[i]-'a'] == 1) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
