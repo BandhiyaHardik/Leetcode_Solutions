11d5bfa877d5933ec16cdedd2d0d1f13973b11be
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-02-10 23:23:08 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/647_Palindromic_Substrings.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+    bool check(string &s, int i, int j) {
+        if(i >= j) {
+            return true;
+        }
+
+        if(s[i] == s[j]) {
+            return check(s, i+1, j-1);
+        }
+
+        return false;
+
+    }
+
+    int countSubstrings(string s) {
+        int n = s.length();
+
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                if(check(s, i, j)) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
