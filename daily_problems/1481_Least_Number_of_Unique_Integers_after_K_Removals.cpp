69f78f0c1280f191e248be15dd8cf4f52c0e17b4
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-02-16 22:23:08 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/1481_Least_Number_of_Unique_Integers_after_K_Removals.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+        unordered_map<int, int> mp;
+        
+        for(int &x : arr) {
+            mp[x]++;
+        }
+
+        vector<int> freq;
+        
+        for(auto &it : mp) {
+            freq.push_back(it.second);
+        }
+        
+        sort(begin(freq), end(freq));
+
+        for(int i = 0; i < freq.size(); i++) {
+            
+            k -= freq[i];
+            
+            if(k < 0) {
+                return freq.size() - i;
+            }
+        }
+        
+        return 0; 
+    }
+};
