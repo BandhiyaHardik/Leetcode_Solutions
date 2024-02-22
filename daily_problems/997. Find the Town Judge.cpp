aeda64e8a7f9a573f1ed63160a624348cce4a5ba
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-02-22 23:34:52 +0530

    "Leetcode_dailly_problem_Solutions"

---
+++ daily_problems/997. Find the Town Judge.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> indegree(n+1);
+        vector<int> outdegree(n+1);
+        
+        for(vector<int> &vec : trust) {
+            indegree[vec[1]]++;
+            outdegree[vec[0]]++;
+        }
+        
+        for(int i = 1; i<n+1; i++) {
+            if(indegree[i] == n-1 && outdegree[i] == 0)
+                return i;
+        }
+        
+        return -1;
+    }
+};
