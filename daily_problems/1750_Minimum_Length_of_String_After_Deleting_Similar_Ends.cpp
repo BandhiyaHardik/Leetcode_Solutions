12488656ba9b177a2bcd9ddccd77a32952671c04
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-03-05 22:46:22 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/1750_Minimum_Length_of_String_After_Deleting_Similar_Ends.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        int n = s.length();
+        
+        int i = 0, j = n-1;
+        
+        while(i < j && s[i] == s[j]) {
+            char ch = s[i];
+            
+            while(i < j && s[i] == ch) {
+                i++;
+            }
+            
+            while(j >= i && s[j] == ch) {
+                j--;
+            }
+        }
+        
+        return j-i+1;
+    }
+};
