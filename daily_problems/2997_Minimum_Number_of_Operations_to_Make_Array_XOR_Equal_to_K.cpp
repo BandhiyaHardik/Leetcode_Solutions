e45d094664731bbdb93efcf5be305f6ecc8c075f
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-04-29 23:34:52 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/2997_Minimum_Number_of_Operations_to_Make_Array_XOR_Equal_to_K.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int totalXor = 0;
+        
+        for(int &num : nums) {
+            totalXor ^= num;
+        }
+        
+        
+        int diff = (totalXor ^ k);
+        
+        return __builtin_popcount(diff);
+        
+    }
+};
