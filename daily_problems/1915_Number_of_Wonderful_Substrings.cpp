b1245a8c023b6f6571ba2aa4c76aa0f3e4a6ded5
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-04-30 22:10:30 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/1915_Number_of_Wonderful_Substrings.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    typedef long long ll;
+    long long wonderfulSubstrings(string word) {
+        unordered_map<ll, ll> mp;
+
+        mp[0] = 1;
+        int cum_xor = 0;
+
+        ll result = 0;
+
+
+        for(char &ch : word) {
+
+            int shift = ch - 'a';
+
+            cum_xor ^= (1 << shift);
+
+            result += mp[cum_xor];
+
+            for(char ch1 = 'a' ; ch1 <= 'j'; ch1++) {
+                shift = ch1 - 'a';
+
+                ll check_xor = (cum_xor ^ (1 << shift));
+
+                result += mp[check_xor];
+            }
+
+            mp[cum_xor]++;
+
+        }
+
+        return result;
+    }
+};
