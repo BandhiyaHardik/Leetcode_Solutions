313b456b47191a9d5e445850017c6d506dedef1c
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-06-11 22:00:31 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/1122_Relative_Sort_Array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        map<int, int> mp;
+
+        for(int &num : arr1) {
+            mp[num]++;
+        }
+
+        int i = 0;
+        for(int &num : arr2) {
+            while(mp[num]-- > 0) {
+                arr1[i++] = num;
+            }
+        }
+
+        for(auto &it : mp) {
+            int freq = it.second;
+            while(freq > 0) {
+                arr1[i++] = it.first;
+                freq--;
+            }
+        }
+
+        return arr1;
+    }
+};
