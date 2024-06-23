a05303a08c2c4ee06cce98db13c165a6829fe1e6
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-06-23 23:26:25 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/1438_Longest_Continuous_Subarray_With_Absolute_Diff_Less_Than_or_Equal_to_Limit.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    typedef pair<int, int> P;
+
+    int longestSubarray(vector<int>& nums, int limit) {
+        int n = nums.size();
+        priority_queue<P> maxPq;
+        priority_queue<P, vector<P>, greater<P>> minPq;
+
+        int i = 0;
+        int j = 0;
+        int maxLength = 0;
+
+        while (j < n) {
+            maxPq.push({nums[j], j});
+            minPq.push({nums[j], j});
+
+            while (maxPq.top().first - minPq.top().first > limit) {
+                i = min(maxPq.top().second, minPq.top().second) + 1;
+
+                while (maxPq.top().second < i) {
+                    maxPq.pop();
+                }
+                while (minPq.top().second < i) {
+                    minPq.pop();
+                }
+            }
+
+            maxLength = max(maxLength, j - i + 1);
+            j++;
+        }
+
+        return maxLength;
+    }
+};
