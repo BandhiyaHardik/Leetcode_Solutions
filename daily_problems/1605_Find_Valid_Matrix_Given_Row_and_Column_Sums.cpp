0e5d0a43200f0c68b27faa4727d6f71933cc53d6
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-07-20 23:33:03 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/1605_Find_Valid_Matrix_Given_Row_and_Column_Sums.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
+        int m = rowSum.size();
+        int n = colSum.size();
+        vector<vector<int>> vec(m, vector<int>(n));
+        
+        int i = 0, j = 0;
+        while(i < m && j < n) {
+            vec[i][j] = min(rowSum[i], colSum[j]);
+
+            rowSum[i] -= vec[i][j];
+            colSum[j] -= vec[i][j];
+
+            if(rowSum[i] == 0) i++;
+            if(colSum[j] == 0) j++;
+            
+        }
+        return vec;
+    }
+};
