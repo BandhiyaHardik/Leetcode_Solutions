9588edeb4326b0b4177064e8dd407a6482a2b294
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-08-01 22:44:06 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/2678_Number_of_Senior_Citizens.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count = 0;
+
+        for (const string& detail : details) {
+
+            string ageStr = detail.substr(11, 2);
+            int age = stoi(ageStr);
+
+            if (age > 60) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
