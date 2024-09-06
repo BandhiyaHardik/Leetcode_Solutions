4ef022c9592b0cf1c2d0567d8db6644f496e35c5
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-09-06 22:57:36 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/3217_Delete_Nodes_From_Linked_List_Present_in_Array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        unordered_set<int> st(begin(nums), end(nums));
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+
+        while (curr != NULL && st.count(curr->val)) {
+            head = curr->next;
+            curr = head;
+        }
+
+        while (curr != NULL) {
+            int currVal = curr->val;
+            if (!st.count(currVal)) {
+                prev = curr;
+                curr = curr->next;
+            } else {
+                prev->next = curr->next;
+                curr = curr->next;
+            }
+        }
+        return head;
+    }
+};
