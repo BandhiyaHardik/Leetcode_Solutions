4a5de912d8ad10921127e38962d755a070a4f35f
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-11-24 22:18:45 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/1975_Maximum_Matrix_Sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        long long sum = 0;
+        int countNegatives = 0; 
+        int smallestAbsoluteValue = INT_MAX;
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                sum += abs(matrix[i][j]);
+
+                countNegatives += matrix[i][j] < 0 ? 1 : 0;
+
+                smallestAbsoluteValue = min(smallestAbsoluteValue, abs(matrix[i][j]));
+
+            }
+        }
+
+        if(countNegatives%2 == 0) {
+            return sum;
+        }
+
+        return sum - 2*smallestAbsoluteValue;
+    }
+};
