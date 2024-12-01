d3bd30c546df04c938d99f8dc132824dedcf0dd2
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-12-01 22:53:58 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/1346_Check_If_N_and_Its_Double_Exist.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        int n = arr.size();
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i != j && arr[i] == 2*arr[j]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+};
