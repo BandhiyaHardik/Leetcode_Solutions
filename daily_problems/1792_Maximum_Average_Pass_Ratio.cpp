8713674d1e17e05bade2af60f2e2b3299528e743
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-12-15 22:30:38 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/1792_Maximum_Average_Pass_Ratio.cpp
@@ -0,0 +1,39 @@
+class Solution {
+    public:
+        #define P pair<double, int>
+        double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+            int n = classes.size();
+    
+            priority_queue<P> pq;
+    
+            for(int i = 0; i < n; i++) {
+                double current_PR = (double)classes[i][0]/classes[i][1];
+                double new_PR = (double)(classes[i][0]+1)/(classes[i][1]+1);
+                double delta = new_PR - current_PR;
+                pq.push({delta, i});
+            }
+
+            while(extraStudents--) {
+                auto curr = pq.top();
+                pq.pop();
+    
+                double delta = curr.first;
+                int idx = curr.second;
+    
+                classes[idx][0]++;
+                classes[idx][1]++;
+    
+                double current_PR = (double)classes[idx][0]/classes[idx][1];
+                double new_PR = (double)(classes[idx][0]+1)/(classes[idx][1]+1);
+                delta = new_PR - current_PR;
+                pq.push({delta, idx});
+            }
+    
+            double result = 0.0;
+            for(int i = 0; i < n; i++) {
+                result += (double)classes[i][0]/classes[i][1];
+            }
+    
+            return result/n;
+        }
+    };
