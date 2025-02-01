a819b1e1c448c801544a27dfc9255a1bc6ddab3d
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2025-02-01 22:35:58 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ 3151_Special_Array_I.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        int n = nums.size();
+
+        for(int i = 0; i <= n-2; i++) {
+            if(nums[i]%2 == nums[i+1]%2) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
