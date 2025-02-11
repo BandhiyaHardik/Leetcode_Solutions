707ef0fa382c49eeaba7a0db4a507370c34dddf2
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2025-02-11 22:53:40 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/1910_Remove_All_Occurrences_of_a_Substring.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        while(true) {
+            int idx = s.find(part);
+
+            if(idx == string::npos) {
+                break;
+            }
+
+            s.erase(idx, part.length());
+        }
+
+        return s;
+    }
+};
