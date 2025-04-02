807b848b6ad192b7a213f6c06c9d2d0e8a396682
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2025-04-02 23:52:20 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/2873_Maximum_Value_of_an_Ordered_Triplet_I.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        int n = nums.size();
+        long long result = 0;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i+1; j < n; j++) {
+                for(int k = j+1; k < n; k++) {
+                    result = max(result, (long long)(nums[i] - nums[j])*nums[k]);
+                }
+            }
+        }
+
+        return result;
+    }
+};
