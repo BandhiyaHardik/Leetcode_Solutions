4d229d9a7f5a87121d98f412eccbf0f20c046e8d
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2025-04-06 22:46:21 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/368_Largest__Divisible_Subset.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        sort(begin(nums), end(nums));
+
+        int n = nums.size();
+        vector<int> t(n, 1);
+
+        vector<int> prev_idx(n, -1);
+        int last_chosen_index = 0;
+        int maxL = 1;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(nums[i]%nums[j] == 0) {
+                    if(t[i] < t[j]+1) {
+                        t[i] = t[j]+1;
+                        prev_idx[i] = j;
+                    }
+
+                    if(t[i] > maxL) {
+                        maxL = t[i];
+                        last_chosen_index = i;
+                    }
+                }
+            }
+        }
+
+        vector<int> result;
+        while(last_chosen_index >= 0) {
+            result.push_back(nums[last_chosen_index]);
+            last_chosen_index = prev_idx[last_chosen_index];
+        }
+
+        return result;
+    }
+};
