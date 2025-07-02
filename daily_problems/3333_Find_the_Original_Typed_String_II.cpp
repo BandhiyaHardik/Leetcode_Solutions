8e2622ff2f8bc5cf268632ca734b7f813332de1e
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2025-07-02 23:53:35 +0530

    "Leetcode_daily_problems_Solutions"

---
+++ daily_problems/3333_Find_the_Original_Typed_String_II.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    int M = 1e9+7;
+
+    int possibleStringCount(string word, int k) {
+        if(k > word.length())
+            return 0;
+        
+        vector<int> freq;
+        int count = 1;
+        for(int i = 1; i < word.length(); i++) {
+            if(word[i] == word[i-1]) {
+                count++;
+            } else {
+                freq.push_back(count);
+                count = 1;
+            }
+        }
+        freq.push_back(count);
+
+        long long P = 1;
+        for(int &f : freq) {
+            P = (P * f) % M;
+        }
+        
+        if(freq.size() >= k) {
+            return P;
+        }
+        int n = freq.size();
+        
+        vector<vector<int>> t(n+1, vector<int>(k+1, 0));
+
+        for(int count = k-1; count >= 0; count--) {
+            t[n][count] = 1;
+        }
+
+        for(int i = n-1; i >= 0; i--) {
+
+            vector<int> prefix(k+1, 0);
+            for(int h = 1; h <= k; h++) {
+                prefix[h] = (prefix[h-1] + t[i+1][h-1]) % M;
+            }
+
+            for(int count = k-1; count >= 0; count--) {
+                
+                int l = count + 1;
+                int r = count + freq[i];
+
+                if(r+1 > k) {
+                    r = k-1;
+                }
+
+                if(l <= r) {
+                    t[i][count] = (prefix[r+1] - prefix[l] + M) % M;
+                }
+                
+            }
+        }
+
+        long long invalidCount = t[0][0];
+
+        return (P - invalidCount + M) % M;
+    }
+};
