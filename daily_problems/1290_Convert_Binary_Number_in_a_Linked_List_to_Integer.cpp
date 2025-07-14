718db225a5d17770616e79d161e0197c78b5bd68
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2025-07-14 23:30:06 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/1290_Convert_Binary_Number_in_a_Linked_List_to_Integer.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    ListNode* reverseLL(ListNode* head) {
+        if(head == NULL || head->next == NULL) {
+            return head;
+        }
+
+        ListNode* last = reverseLL(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return last;
+    }
+
+    int getDecimalValue(ListNode* head) {
+        head= reverseLL(head);
+
+        int result = 0;
+        int power = 0;
+        while(head) {
+            if(head->val == 1) {
+                result += pow(2, power);
+            }
+            power++;
+            head = head->next;
+        }
+
+        return result;
+    }
+};
