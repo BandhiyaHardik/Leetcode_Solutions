309bad5c3f7ddf03d03d43961c64590e57446e6e
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2025-07-16 22:47:54 +0530

    "Leetcode_daily_problem_SOlutions"

---
+++ daily_problems/3201_Find_the_Maximum_Length_of_Valid_Subsequence_I.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maximumLength(vector<int>& nums) {
+        int countEven = 0, countOdd = 0;
+        for (int num : nums) {
+            if (num % 2 == 0) countEven++;
+            else countOdd++;
+        }
+
+        int altLen = 1;
+        int prevParity = nums[0] % 2;
+
+        for (int i = 1; i < nums.size(); ++i) {
+            int currParity = nums[i] % 2;
+            if (currParity != prevParity) {
+                altLen++;
+                prevParity = currParity;
+            }
+        }
+
+        return max({countEven, countOdd, altLen});
+    }
+};
