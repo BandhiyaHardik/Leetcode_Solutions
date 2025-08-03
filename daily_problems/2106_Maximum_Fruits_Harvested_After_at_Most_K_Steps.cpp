1b2096a4d7ce5271ce1887b596e0965b3d6e72cc
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2025-08-03 23:34:59 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/2106_Maximum_Fruits_Harvested_After_at_Most_K_Steps.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int maxTotalFruits(vector<vector<int>>& fruits, int startPos, int k) {
+        int n = fruits.size();
+        vector<int> prefixSum(n);
+        vector<int> indices(n);
+
+        for (int i = 0; i < n; i++) {
+            indices[i]   = fruits[i][0];
+            prefixSum[i] = fruits[i][1] + (i > 0 ? prefixSum[i - 1] : 0);
+        }
+
+        int maxFrutis = 0;
+
+        for (int d = 0; d <= k / 2; d++) {
+            int remain = k - 2 * d;
+            int i   = startPos - d;
+            int j  = startPos + remain;
+
+            int left  = lower_bound(indices.begin(), indices.end(), i) - indices.begin();
+            int right = upper_bound(indices.begin(), indices.end(), j) - indices.begin() - 1;
+
+            if(left <= right) {
+                int total = prefixSum[right] - (left > 0 ? prefixSum[left - 1] : 0);
+                maxFrutis = max(maxFrutis, total);
+            }
+
+            i  = startPos - remain;
+            j  = startPos + d;
+            
+            left  = lower_bound(indices.begin(), indices.end(), i) - indices.begin();
+            right = upper_bound(indices.begin(), indices.end(), j) - indices.begin() - 1;
+
+            if(left <= right) {
+                int total = prefixSum[right] - (left > 0 ? prefixSum[left - 1] : 0);
+                maxFrutis = max(maxFrutis, total);
+            }
+        }
+
+        return maxFrutis;
+    }
+};
