ce95e1347c9d2b93f7827b9dae701f573c6f2a82
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2025-08-13 22:36:09 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/326_Power_of_Three.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n <= 0) {
+            return false;
+        }
+        else if(n == 1) {
+            return true;
+        }
+        return n%3 == 0 && isPowerOfThree(n/3);  
+    }
+};
