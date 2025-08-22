dbdcdf9c1c6496301175602a99f160940461c120
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2025-08-22 19:51:13 +0530

    "Leetcode_daily_problem_Solutions"

---
+++ daily_problems/3195_Find_the_Minimum_Area_to_Cover_All_Ones_I.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minimumArea(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        int minRow = m;
+        int maxRow = -1;
+        int minCol = n;
+        int maxCol = -1;
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == 1) {
+                    minRow = min(minRow, i);
+                    maxRow = max(maxRow, i);
+
+                    minCol = min(minCol, j);
+                    maxCol = max(maxCol, j);
+                }       
+            }
+        }
+        return (maxRow - minRow + 1) * (maxCol - minCol + 1);
+    }
+};
